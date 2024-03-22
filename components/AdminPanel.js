e2ae433517dbe5c71bdfde3e2b61e842e9710ee1
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 15:57:08 +0100

    have dropdown

---
+++ components/AdminPanel.js
@@ -0,0 +1,11 @@
+export const AdminPanel = () => {
+	return /*html*/ `
+<section class="admin">
+	<select class="backgroundColorSelection">
+		<option value="beige">Beige</option>
+		<option value="#ccc">Light gray</option>
+		<option value="#888">Dark gray</option>
+	</select> 
+</section>
+	`;
+};
