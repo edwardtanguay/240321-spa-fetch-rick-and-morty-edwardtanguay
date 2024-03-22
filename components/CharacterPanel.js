2dae5367289509e82c7d50e1b27c1cfc11c7d78a
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-22 15:31:42 +0100

    finished functionality

--- components/CharacterPanel.js
+++ components/CharacterPanel.js
@@ -1,7 +1,6 @@
 import * as tools from '../tools.js';
 
 export const CharacterPanel = (characterElem, character) => {
-
 	characterElem.innerHTML = `
 		<h1>${tools.capitalizeAllLetters(character.name)}</h1>
 		<div>
--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-// import { getRandomIndex } from './tools.js'; // 1. import individual functions
-import * as tools from './tools.js'; // 2. import namespace "tools" with intellisense
-// import { getRandomIndex } from './tools.js'; // 3. import indvidiaul function as the default
-// import  capitalizeAllLetters from './tools.js';
-import * as appModel from './appModel.js';
-import { CharacterPanel } from './components/CharacterPanel.js';
+import * as site from './site.js';
 
 const characterElem = document.querySelector('.character');
 const btnReloadElem = document.querySelector('.reload');
 
 btnReloadElem.addEventListener('click', () => {
-
+	site.renderSite(characterElem);
 });
 
-characterElem.innerHTML = `
-	<h1>Loading...</h1>
-	`;
+site.renderSite(characterElem);
 
-try {
-	const characters = await appModel.getCharacters();
-	const randomIndex = tools.getRandomIndex(characters.length);
-	const character = characters[randomIndex];
-	CharacterPanel(characterElem, character);
-}
-catch (e) {
-	characterElem.innerHTML = e.message;
-}
